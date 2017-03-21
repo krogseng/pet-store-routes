79d8d5cb74840696adb49879c57020070b537ad3
Lynn <code@krogseng.com>
2017-03-20 17:44:17 -0700

    Stores added.

---
+++ src/components/Stores.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Stores(props) {
+    return (
+        <div>
+            <h1>Pet Stores in Your Town</h1>
+            <ul>
+                {props.stores.map(store => {
+                    return (
+                        <li key={store.id}>
+                            <Link to={`/stores/${store.id}`}>
+                                {store.location}
+                            </Link>
+                        </li>
+                    )
+                })
+                }
+            </ul>
+        </div>
+    )
+
+}
+export default Stores;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,49 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import Home from '../components/Home';
+import Stores from '../components/Stores';
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stores: [{
+        id: 1,
+        location: 'downtown',
+      },
+      {
+        id: 2,
+        location: 'midtown',
+      },
+      {
+        id: 3,
+        location: 'uptown',
+      }],
+      pets: [{
+        id: 11,
+        storeId: 1,
+        type: 'cat'
+      },
+      {
+        id: 12,
+        storeId: 1,
+        type: 'parakeet'
+      },
+      {
+        id: 14,
+        storeId: 1,
+        type: 'sloth'
+      },]
+    }
+  }
   render() {
     return (
       <div>
         <Route exact path='/' component={Home} />
+        <Route path='/stores' render={(props) => (
+          <Stores { ...props } stores={this.state.stores} pets={this.state.pets} />
+        )} />
       </div>
     );
   }
